5a2bab519fd0aebdbbf92d40f821ffead5ed26f7
t-tkmr <tetsuro.takamura@gmail.com>
2023-01-08 23:04:57 +0900

    add tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import "./App.css";
 
-function App() {
-  return (
-    <>
+import { useState } from "react";
 
-    </>
+function App() {
+  const [todo, setTodo] = useState([]);
+  const [v, setV] = useState("");
+  const clickButton = (e) => {
+    setTodo((prevTodo) => {
+      return [...prevTodo, { id: 1, value: v }];
+    });
+  };
+  const getInput = (e) => {
+    setV(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <input value={v} onChange={getInput}></input>
+      <button onClick={clickButton}>追加</button>
+      <div>
+        {todo.map((todoitem) => (
+          <>
+            <div> {todoitem.id}</div>
+
+            <div> {todoitem.value}</div>
+          </>
+        ))}
+      </div>
+    </div>
   );
 }
-
 export default App;
